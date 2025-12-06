9a8cff5b1772319085e7fd7241bbc81017a30fc6
cmdelite <elitebitvavo@gmail.com>
2025-12-06 13:27:01 +0400

    [gta4xl] | The Clover Project: Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2025 The Clover Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4xl.mk
+    $(LOCAL_DIR)/clover_gta4xl.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gta4xl-eng \
-    lineage_gta4xl-user \
-    lineage_gta4xl-userdebug
+    clover_gta4xl-eng \
+    clover_gta4xl-user \
+    clover_gta4xl-userdebug
