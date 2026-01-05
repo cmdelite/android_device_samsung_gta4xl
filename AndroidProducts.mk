8d1a3286f197b183ad7665070bfbb03cef28ac9d
cmdelite <elitebitvavo@gmail.com>
2026-01-05 16:59:09 +0400

    [gta4xl] | YAAP: Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The YAAP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4xl.mk
+    $(LOCAL_DIR)/yaap_gta4xl.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gta4xl-eng \
-    lineage_gta4xl-user \
-    lineage_gta4xl-userdebug
+    yaap_gta4xl-eng \
+    yaap_gta4xl-user \
+    yaap_gta4xl-userdebug
--- yaap_gta4xl.mk
+++ yaap_gta4xl.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020-2021 The LineageOS Project
+# Copyright (C) 2020-2021 The YAAP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,12 +26,12 @@ $(call inherit-product, device/samsung/gta4xl/device.mk)
 TARGET_SCREEN_HEIGHT := 2000
 TARGET_SCREEN_WIDTH := 1200
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
+## Inherit some common YAAP stuff
+$(call inherit-product, vendor/yaap/config/common_full_tablet.mk)
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := gta4xl
-PRODUCT_NAME := lineage_gta4xl
+PRODUCT_NAME := yaap_gta4xl
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-P615
 PRODUCT_MANUFACTURER := samsung
